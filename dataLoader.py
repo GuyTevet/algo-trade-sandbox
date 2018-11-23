c25d285a87f6193eb33fe75fd7c62c84090732e4
GuyTevet <guy.tvt@gmail.com>
2018-11-23 13:25:47 +0200

    dataLoader - define data structure trader fix

--- dataLoader.py
+++ dataLoader.py
@@ -9,9 +9,32 @@ class DataLoader(object):
     load csv file to train/test dictionaries
     """
 
-    def __init__(self,dataPath):
-        self.dataPath = dataPath
-        self.testSize = 200 # HARD CODED - do not change!
+    def __init__(self,data_path):
+        self.data_path = data_path
+        self.test_size = 200 # HARD CODED - do not change!
+        self.train_dict = {}
+        self.test_dict = {}
+
+    def load_data(self):
+
+        num_stocks = 0
+        num_dates = 0
+
+        self.train_dict = {
+            'num_stocks': num_stocks,
+            'num_dates': num_dates,
+            'close': np.empty([num_stocks,num_dates], dtype=np.float32),
+            'open': np.empty([num_stocks, num_dates], dtype=np.float32),
+            'high': np.empty([num_stocks, num_dates], dtype=np.float32),
+            'low': np.empty([num_stocks, num_dates], dtype=np.float32),
+            'volume': np.empty([num_stocks, num_dates], dtype=np.float32)
+        }
+
+        self.test_dict = copy(self.train_dict)
+
+        return
+
+
 
     # def splitData(self,splitList):
     #
@@ -41,42 +64,38 @@ class DataLoader(object):
     #
     #
     #     return names, dates
-
-
-
-
-
-    def getData(self):
-
-        data = {}
-
-        with open(self.dataPath,'r') as f:
-            reader = csv.DictReader(f)
-
-            for row in reader:
-
-                newStock = False
-                name = row['Name']
-                del row['Name']
-
-                # add name if new
-                if name not in data.keys():
-                    data[name] = {}
-                    newStock = True
-
-                # add data
-                for key in row.keys():
-                    if newStock:
-                        data[name][key] = [row[key]]
-                    else:
-                        data[name][key].append(row[key])
-
-        # split to train/test
-        totalSize = len(data[name][key])
-        trainSize = totalSize - self.testSize
-        # TODO
-
-        return data
+    #
+    # def getData(self):
+    #
+    #     data = {}
+    #
+    #     with open(self.dataPath,'r') as f:
+    #         reader = csv.DictReader(f)
+    #
+    #         for row in reader:
+    #
+    #             newStock = False
+    #             name = row['Name']
+    #             del row['Name']
+    #
+    #             # add name if new
+    #             if name not in data.keys():
+    #                 data[name] = {}
+    #                 newStock = True
+    #
+    #             # add data
+    #             for key in row.keys():
+    #                 if newStock:
+    #                     data[name][key] = [row[key]]
+    #                 else:
+    #                     data[name][key].append(row[key])
+    #
+    #     # split to train/test
+    #     totalSize = len(data[name][key])
+    #     trainSize = totalSize - self.testSize
+    #     # TODO
+    #
+    #     return data
 
 # D = DataLoader('./data/all_stocks_5yr.csv')
 # data = D.getData()
--- trader.py
+++ trader.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+
 class Trader(object):
 
     def __init__(self):
         pass
 
-    def predict(self,historyDict):
+    def predict(self, history_dict):
 
-        prediction = (1 / historyDict['numStocks']) * np.ones([historyDict['numStocks']],dtype=np.float32)
+        prediction = (1 / history_dict['num_stocks']) * np.ones([history_dict['num_stocks']],dtype=np.float32)
 
         return prediction
 
